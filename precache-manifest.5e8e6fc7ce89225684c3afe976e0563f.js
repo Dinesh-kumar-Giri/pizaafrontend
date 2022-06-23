fc3c6b4a5ee524881fec98092017695383248e88
Dinesh <dineshgiri1542@gmail.com>
2022-06-23 12:03:39 +0530

    Updates

---
+++ precache-manifest.5e8e6fc7ce89225684c3afe976e0563f.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd5ff5310712cb698070365b513a6de8",
+    "url": "/pizzaFrontend/index.html"
+  },
+  {
+    "revision": "b60a83595028d1138622",
+    "url": "/pizzaFrontend/static/css/main.befaa53a.chunk.css"
+  },
+  {
+    "revision": "b4031ae64fafdd5d7e66",
+    "url": "/pizzaFrontend/static/js/2.8b1e1496.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pizzaFrontend/static/js/2.8b1e1496.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b60a83595028d1138622",
+    "url": "/pizzaFrontend/static/js/main.161d6228.chunk.js"
+  },
+  {
+    "revision": "fc723eec4afc3065a699",
+    "url": "/pizzaFrontend/static/js/runtime-main.a863e784.js"
+  },
+  {
+    "revision": "84bc78e2cd3f9011d6c3054facc1e2fe",
+    "url": "/pizzaFrontend/static/media/bg.84bc78e2.jpg"
+  },
+  {
+    "revision": "ad44f98fd7fd7dc7650e7893ac2d7ddb",
+    "url": "/pizzaFrontend/static/media/img6.ad44f98f.jpg"
+  },
+  {
+    "revision": "e38280173437430a99c66f1e207cc1f8",
+    "url": "/pizzaFrontend/static/media/img9.e3828017.jpg"
+  },
+  {
+    "revision": "337c866f059cdfcc1368dba54d45f9a4",
+    "url": "/pizzaFrontend/static/media/pizza.337c866f.jpg"
+  },
+  {
+    "revision": "d101237b5188b4fbd20a4a9b6824062a",
+    "url": "/pizzaFrontend/static/media/pizza1.d101237b.png"
+  },
+  {
+    "revision": "ace97dc2b3f5e0d95f51a7da16c48d83",
+    "url": "/pizzaFrontend/static/media/pizza2.ace97dc2.png"
+  },
+  {
+    "revision": "f9d26f1702947d07bb07e54967c55fd6",
+    "url": "/pizzaFrontend/static/media/pizza3.f9d26f17.png"
+  },
+  {
+    "revision": "2096a09a0055909f9cd782b69f359e21",
+    "url": "/pizzaFrontend/static/media/pizza4.2096a09a.png"
+  },
+  {
+    "revision": "ba6c807bd8af1327b0d56ce603f2baf8",
+    "url": "/pizzaFrontend/static/media/pizza5.ba6c807b.png"
+  },
+  {
+    "revision": "6c87c0ad713bf58542c0b4ec394d0124",
+    "url": "/pizzaFrontend/static/media/pizza6.6c87c0ad.png"
+  }
+]);
